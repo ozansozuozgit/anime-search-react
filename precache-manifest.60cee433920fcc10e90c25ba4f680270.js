d02fa50c12d1d0b65699422882d8aab133a3241d
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-10-25 20:39:53 +0300

    Updates

---
+++ precache-manifest.60cee433920fcc10e90c25ba4f680270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c26c8abda279b8f3a15847edbd3f0e",
+    "url": "/anime-search-react/index.html"
+  },
+  {
+    "revision": "ee300c798007cfe6d6b4",
+    "url": "/anime-search-react/static/css/main.1bbd674f.chunk.css"
+  },
+  {
+    "revision": "d73c2b372497dd31ca1e",
+    "url": "/anime-search-react/static/js/2.2d25cf87.chunk.js"
+  },
+  {
+    "revision": "3fc67584a795169bc77165cef3b6afa4",
+    "url": "/anime-search-react/static/js/2.2d25cf87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee300c798007cfe6d6b4",
+    "url": "/anime-search-react/static/js/main.aeda9b21.chunk.js"
+  },
+  {
+    "revision": "ba1658e790421b2cce45",
+    "url": "/anime-search-react/static/js/runtime-main.cde99b66.js"
+  }
+]);
